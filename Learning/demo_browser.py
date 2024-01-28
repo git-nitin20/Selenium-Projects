d417780196c668448907811fdd5f5449632cd12c
git-nitin20 <godfu2000@gmail.com>
2024-01-28 23:00:50 +0530

    learned webdriver

---
+++ Learning/demo_browser.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service_obj = Service()
+driver = webdriver.Chrome(service=service_obj)
+driver.get('https://www.youtube.com/')
+
+print(driver.title)
